aacd7b2548dad4ceb89c6cf482bf75dcba22e94c
a11koval <153006026+a11koval@users.noreply.github.com>
2025-01-03 16:20:03 +0100

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let result = 5 + 5 + '5'; 
+console.log(`Result: ${result}, Type of result: ${typeof result}`); 
+
+
+let email = "antonkov2007aa@gmail.com"; 
+let containsAt = email.includes('@');
+let emailLength = email.length; 
+console.log(`Email містить '@': ${containsAt}, кількість символів у email: ${emailLength}`);
+
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = `${word1} ${word2} ${word3} Viktor`; 
+console.log(`Повне ім'я: ${fullName}`);
+
+
+let userName = "Олександро"; 
+let payment = 300; 
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
